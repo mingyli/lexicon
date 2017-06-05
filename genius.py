60a8ddb3baa6187d642bc04a84bc25cd477d41ba
ming li <mingyli34@gmail.com>
2017-06-04 20:50:14 -0400

    writes album lyrics to file correctly

--- genius.py
+++ genius.py
@@ -5,6 +5,7 @@ import re
 from bs4 import BeautifulSoup
 
 base_url = 'https://api.genius.com'
+search_url = base_url + '/search'
 TOKEN = os.environ['TOKEN']
 headers = {'Authorization': 'Bearer ' + TOKEN}
 
@@ -31,22 +32,23 @@ def parse_album_file(album_file):
 if __name__ == '__main__':
     """
     command line arguments: album.txt, target_directory
+    $ python genius.py lyrics/kendrick/tpab.txt lyrics/kendrick/tpab/
+    that last slash / in target_directory is important
     """
     assert len(sys.argv) == 3
     _, album_file, target_directory = sys.argv
     artist_name, album_name, songs = parse_album_file(album_file)
-    print(artist_name)
-    print(album_name)
-    print(songs)
-    """
-    search_url = base_url + '/search'
-    data = {'q': song_title}
-    response = requests.get(search_url, data=data, headers=headers)
-    json = response.json()
-    for hit in json['response']['hits']:
-        if hit['result']['primary_artist']['name'] == artist_name:
-            song_api_path = hit['result']['api_path']
-            lyrics = lyrics_from_song_api_path(song_api_path)
-            print(lyrics)
-            break
-            """
+    for i, song_title in enumerate(songs):
+        data = {'q': song_title + ' ' + artist_name}
+        response = requests.get(search_url, data=data, headers=headers)
+        json = response.json()
+        for hit in json['response']['hits']:
+            if hit['result']['primary_artist']['name'] == artist_name:
+                song_api_path = hit['result']['api_path']
+                lyrics = lyrics_from_song_api_path(song_api_path)
+                # print(lyrics)
+                f = open(target_directory + str(i) + '.txt', 'w')
+                f.write(song_title + '\n\n')
+                f.write(lyrics)
+                f.close()
+                break
