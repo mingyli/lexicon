e1d75580e0d71f8b31670c33eecb7e004311055a
ming li <mingyli34@gmail.com>
2017-06-04 20:21:51 -0400

    parse album file correctly

--- genius.py
+++ genius.py
@@ -1,13 +1,12 @@
 import requests
 import os
+import sys
 import re
 from bs4 import BeautifulSoup
 
 base_url = 'https://api.genius.com'
 TOKEN = os.environ['TOKEN']
 headers = {'Authorization': 'Bearer ' + TOKEN}
-song_title = "Alright"
-artist_name = "Kendrick Lamar"
 
 def lyrics_from_song_api_path(song_api_path):
     song_url = base_url + song_api_path
@@ -18,12 +17,28 @@ def lyrics_from_song_api_path(song_api_path):
     page = requests.get(page_url)
     html = BeautifulSoup(page.text, 'html.parser')
     [h.extract() for h in html('script')]
-    # import pdb; pdb.set_trace()
-    # lyrics = html.find('lyrics').get_text()
     lyrics = html.find('div', {'class': 'lyrics'}).get_text()
     return re.sub(r'\n\[.*\]', '', lyrics)
 
+def parse_album_file(album_file):
+    txt = open(album_file)
+    artist_name = txt.readline().strip()
+    album_name = txt.readline().strip()
+    songs = [line.strip() for line in txt]
+    txt.close()
+    return artist_name, album_name, songs
+
 if __name__ == '__main__':
+    """
+    command line arguments: album.txt, target_directory
+    """
+    assert len(sys.argv) == 3
+    _, album_file, target_directory = sys.argv
+    artist_name, album_name, songs = parse_album_file(album_file)
+    print(artist_name)
+    print(album_name)
+    print(songs)
+    """
     search_url = base_url + '/search'
     data = {'q': song_title}
     response = requests.get(search_url, data=data, headers=headers)
@@ -34,3 +49,4 @@ if __name__ == '__main__':
             lyrics = lyrics_from_song_api_path(song_api_path)
             print(lyrics)
             break
+            """
