4823c4f60eccffddcb626ccc844f9900591d8be8
ming li <mingyli34@gmail.com>
2017-06-04 23:06:48 -0400

    lexer freq dist

---
+++ README.md
@@ -0,0 +1,7 @@
+my attempts at playing around with NLTK
+
+## `genius.py`
+
+uses BeautifulSoup and Genius API to retrieve lyrics 
+
+requires a Genius API token. I set mine as an environment variable using `export TOKEN=<token>`
--- lexer.py
+++ lexer.py
@@ -1,6 +1,14 @@
+from nltk import FreqDist
 from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus import stopwords
 
 if __name__ == '__main__':
     corpus_root = 'lyrics/kendrick/tpab/'
-    wordlists = PlaintextCorpusReader(corpus_root, '.*\.txt')
-    print(wordlists.fileids())
+    album_corpus = PlaintextCorpusReader(corpus_root, r'.*\.txt')
+    words = album_corpus.words()
+    # use a set for stopwords for faster lookup
+    stopword_set = set(stopwords.words('english'))
+    # TODO: remove punctuation
+    words = [word for word in words if word.lower() not in stopword_set]
+    fdist = FreqDist(words)
+    fdist.most_common(50)
